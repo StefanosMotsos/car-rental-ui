742701d3b4e2529f8e869174b8969ca3a999f7fe
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-06-06 19:34:21 +0300

    chore: added enter functionality

--- src/components/shared/SearchBar.tsx
+++ src/components/shared/SearchBar.tsx
@@ -16,6 +16,7 @@ const SearchBar = ({ onSearch }: SearchBarProps) => {
                 <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-zinc-400 pointer-events-none"/>
                 <input type="text"
                        value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+                       onKeyDown={(e) => { if (e.key === "Enter") onSearch(inputValue) }}
                        className="w-full rounded-xl bg-zinc-800 border border-zinc-500 text-zinc-400 py-2 pl-9"
                        placeholder="Search Vehicles" />
                 <Button
